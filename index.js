6709d4e5e27a0bb347daaa23027068471c36c0ee
amaan-sde <amaankhan172595@gmail.com>
2026-09-04 22:14:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// route -> 
+
+// CRUD opeartion -> create , retrieve , update , delete 
+
+// methods : -> post , get , put , delete 
+
+app.get('/', (req, res) => {
+    res.send("hello ji kaise hai aap!")
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`server is running at ${PORT}`)
+})
+
